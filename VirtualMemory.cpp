7c6ccf78ab301b2a4a12ec13d004fff4696be695
evyatarkar <80752596+evyatarkar@users.noreply.github.com>
2022-05-20 19:36:46 +0300

    End of Day One

--- VirtualMemory.cpp
+++ VirtualMemory.cpp
@@ -12,7 +12,8 @@ int getStartBit (int level)
   return OFFSET_WIDTH * (TABLES_DEPTH - level);
 }
 
-uint64_t getSubAddress (uint64_t virtualAddress, int level, int bitWidth=OFFSET_WIDTH)
+uint64_t
+getSubAddress (uint64_t virtualAddress, int level, int bitWidth = OFFSET_WIDTH)
 {
   int startInd = getStartBit (level);
   uint64_t mask = 1LL << (bitWidth);
@@ -43,76 +44,111 @@ int isFrameEmpty (uint64_t frameIndex)
   return 1;
 }
 
-int getDistanceFromCurFrameIndex(uint64_t curFrameIndex, uint64_t logicalAddressPageIndex)
+int
+getDistanceFromCurFrameIndex (uint64_t curLogicalAddressIndex, uint64_t logicalAddressPageIndex)
 {
   int a, b;
-  a = abs((int) (logicalAddressPageIndex - curFrameIndex));
+  a = abs ((int) (logicalAddressPageIndex - curLogicalAddressIndex));
   b = NUM_PAGES - a;
-  if (a < b){
-    return a;
-  }
+  if (a < b)
+    {
+      return a;
+    }
   return b;
 }
 
-void DFS (uint64_t fatherFrameIndex, uint64_t curFrameIndex, int level, uint64_t *res,
-          int *maxDist, uint64_t *maxDistIndex, uint64_t logicalAddressPageIndex)
+void
+DFS (uint64_t fatherFrameIndex, uint64_t curFrameIndex, int level, uint64_t *res,
+     int *maxDist, uint64_t *maxDistIndex, uint64_t logicalAddressPageIndex,
+     uint64_t *fatherIndexOfFinalIndex, uint64_t *logicalAddressOfFinalFrameIndex,
+     uint64_t *frameIndexOfFinalIndexInFather)
 {
 
   // makes sure to NOT delete the current frame
+  auto currentLogicalAddress = getSubAddress (curFrameIndex, TABLES_DEPTH - 1,
+                                              VIRTUAL_ADDRESS_WIDTH
+                                              - OFFSET_WIDTH);
   if (fatherFrameIndex != curFrameIndex)
     {
       if (isFrameEmpty (curFrameIndex))
         {
           *res = curFrameIndex;
+          *logicalAddressOfFinalFrameIndex = currentLogicalAddress;
           return;
         }
     }
 
   if (level == TABLES_DEPTH)
     {
-      auto dist = reinterpret_cast<unsigned long>(getDistanceFromCurFrameIndex);
-      if (*maxDist < dist){
+      auto dist = getDistanceFromCurFrameIndex (currentLogicalAddress, logicalAddressPageIndex);
+      if (*maxDist < dist)
+        {
           *maxDist = dist;
           *maxDistIndex = curFrameIndex;
-      }
+          *logicalAddressOfFinalFrameIndex = currentLogicalAddress;
+        }
     }
 
-    for (int i = 0; i < PAGE_SIZE; i++)
-      {
-        word_t *value;
-        PMread (curFrameIndex * PAGE_SIZE + i, value);
-        if (*value == 0)
-          {
-            DFS (fatherFrameIndex, (uint64_t) value, level + 1, res, maxDist, maxDistIndex, logicalAddressPageIndex);
-          }
-      }
+  for (int i = 0; i < PAGE_SIZE; i++)
+    {
+      word_t *value;
+      PMread (curFrameIndex * PAGE_SIZE + i, value);
+      uint64_t *indexInFather;
+      *indexInFather = i;
+      if (*value == 0)
+        {
+          DFS (fatherFrameIndex, (uint64_t) value, level + 1, res,
+               maxDist, maxDistIndex, logicalAddressPageIndex,
+               &curFrameIndex, logicalAddressOfFinalFrameIndex, indexInFather);
+        }
+    }
 }
 
-uint64_t findFreeFrameIndex (uint64_t fatherFrameIndex, int *nextIndex, uint64_t logicalAddressPageIndex)
+uint64_t
+findFreeFrameIndex (uint64_t fatherFrameIndex, int *nextIndex, uint64_t logicalAddressPageIndex)
 {
   if (*nextIndex + 1 < NUM_FRAMES)
     {
-      return ++(*nextIndex); // todo: check that ++ before return not after
+      (*nextIndex)++;
+      return (*nextIndex); // todo: check that ++ before return not after
     }
   uint64_t *res;
   *res = -1;
   int *maxDist;
   *maxDist = -1;
+  uint64_t *logicalAddressOfFinalFrameIndex;
   uint64_t *maxDistIndex;
-  DFS (fatherFrameIndex, 0, 0, res, maxDist, maxDistIndex, logicalAddressPageIndex);
-  if (*res != -1){
-    return *res;
-  }
+  uint64_t *fatherIndexOfFinalIndex;
+  uint64_t *frameIndexOfFinalIndexInFather;
+  DFS (fatherFrameIndex, 0, 0, res, maxDist, maxDistIndex,
+       logicalAddressPageIndex, fatherIndexOfFinalIndex, logicalAddressOfFinalFrameIndex,
+       frameIndexOfFinalIndexInFather);
+  if (*res != -1)
+    {
+      PMevict (*res, logicalAddressPageIndex);
+      PMwrite ((*fatherIndexOfFinalIndex) * PAGE_SIZE
+               + *frameIndexOfFinalIndexInFather, 0);
+      return *res;
+    }
+  PMevict (*maxDistIndex, logicalAddressPageIndex);
+  initializeTable (*maxDistIndex);
+  PMwrite ((*fatherIndexOfFinalIndex) * PAGE_SIZE
+           + *frameIndexOfFinalIndexInFather, 0);
   return *maxDistIndex;
 }
 
-word_t getFinalPage (uint64_t virtualAddress)
+uint64_t insertPageToFrame (uint64_t virtualAddress)
 {
   uint64_t p_i;
-  int nextFreeIndex = 0;
+  int *nextFreeIndex;
+  *nextFreeIndex = 0;
   word_t addresses[TABLES_DEPTH + 1];
   addresses[0] = 0;
-  uint64_t logicalAddressPageIndex = getSubAddress(virtualAddress, TABLES_DEPTH-1, VIRTUAL_ADDRESS_WIDTH-OFFSET_WIDTH);
+  uint64_t freeFrameIndex;
+  uint64_t logicalAddressPageIndex = getSubAddress (virtualAddress,
+                                                    TABLES_DEPTH - 1,
+                                                    VIRTUAL_ADDRESS_WIDTH
+                                                    - OFFSET_WIDTH);
   for (int i = 0; i < TABLES_DEPTH; i++)
     {
       p_i = getSubAddress (virtualAddress, i);
@@ -120,11 +156,13 @@ word_t getFinalPage (uint64_t virtualAddress)
       if (addresses[i + 1] == 0)
         {
           // find room for next kid
-          uint64_t freePageTableIndex = findFreeFrameIndex (addresses[i], &nextFreeIndex, logicalAddressPageIndex);
-//        initializeTable(freePageTableIndex);
+          freeFrameIndex = findFreeFrameIndex (addresses[i], nextFreeIndex, logicalAddressPageIndex);
+          // link the new empty frame to father
+          PMwrite (addresses[i] * PAGE_SIZE + p_i, freeFrameIndex);
         }
     }
-  return addresses[TABLES_DEPTH];
+  PMrestore (freeFrameIndex, logicalAddressPageIndex);
+  return freeFrameIndex;
 }
 
 /*
@@ -142,7 +180,12 @@ void VMinitialize ()
  * returns 0 on failure (if the address cannot be mapped to a physical
  * address for any reason)
  */
-int VMread (uint64_t, word_t *value);
+int VMread (uint64_t virtualAddress, word_t *value)
+{
+  uint64_t newFrameIndex = insertPageToFrame(virtualAddress);
+  auto d = getSubAddress(virtualAddress, TABLES_DEPTH);
+  PMread(newFrameIndex * PAGE_SIZE + d, value);
+}
 
 /* Writes a word to the given virtual address.
  *
@@ -150,16 +193,20 @@ int VMread (uint64_t, word_t *value);
  * returns 0 on failure (if the address cannot be mapped to a physical
  * address for any reason)
  */
-int VMwrite (uint64_t virtualAddress, word_t value);
-
-int main (int argc, char **argv)
-{
-  uint64_t add = 57;
-  uint64_t add1 = 60;
-//  auto res = getSubAddress (add, 4);
-//  std::cout << res << " " << add << std::endl;
-
-  auto r = getDistanceFromCurFrameIndex(add, add1);
-  std::cout << r << " " << std::endl;
-
+int VMwrite (uint64_t virtualAddress, word_t value){
+  uint64_t newFrameIndex = insertPageToFrame(virtualAddress);
+  auto d = getSubAddress(virtualAddress, TABLES_DEPTH);
+  PMwrite(newFrameIndex * PAGE_SIZE + d, value);
 }
+//
+//int main (int argc, char **argv)
+//{
+//  uint64_t add = 57;
+//  uint64_t add1 = 60;
+////  auto res = getSubAddress (add, 4);
+////  std::cout << res << " " << add << std::endl;
+//
+//  auto r = getDistanceFromCurFrameIndex (add, add1);
+//  std::cout << r << " " << std::endl;
+//
+//}
